1aabaa66050af2d55139d908fc67c9f3961a08a4
Xudong Yang <fanderous@gmail.com>
2017-07-11 23:07:29 +0800

    remove endless from dependency

--- core/core.go
+++ core/core.go
@@ -1,9 +1,14 @@
 package core
 
 import (
+	"context"
 	"crypto/tls"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"path/filepath"
+	"time"
 
 	"github.com/fvbock/endless"
 	"github.com/gin-gonic/gin"
@@ -133,7 +138,23 @@ func (s *EZServer) ListenAndServe() {
 			server.TLSConfig = &tls.Config{GetCertificate: s.am.GetCertificate}
 			server.Handler = s
 		}
-		server.ListenAndServeTLS("", "")
+		go func() {
+			if err := server.ListenAndServeTLS("", ""); err != nil {
+				log.Printf("listen: %s\n", err)
+			}
+		}()
+		// Wait for interrupt signal to gracefully shutdown the server with a timeout of 5 seconds.
+		quit := make(chan os.Signal)
+		signal.Notify(quit, os.Interrupt)
+		<-quit
+		log.Println("Shutdown Server ...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := server.Shutdown(ctx); err != nil {
+			log.Fatal("Server Shutdown:", err)
+		}
+		log.Println("Server exited")
 	} else {
 		server := endless.NewServer(s.selectAddr(), s)
 		server.ListenAndServeTLS(*s.crtFile, *s.keyFile)
